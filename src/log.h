0d2f7cf11714ec2e27e0a5f6141e845fdb046349
HSPMDev15 <unidadcdeveloper@gmail.com>
2026-07-22 18:36:32 -0500

    Task: create log header

---
+++ src/log.h
@@ -0,0 +1,43 @@
+#pragma once
+
+// Prupose: console output with semantic levels (did it mainly to avoid greedy .c_str() and print directly the literal strings)
+// Error/Warning go to stderr with color,info goes to stdout (the normal output of the program)
+
+#include "Formatting.h"
+#include <cstdio>
+#include <string_view>
+
+namespace log_detail {
+
+inline constexpr std::string_view kRed    = "\x1b[31m";
+inline constexpr std::string_view kYellow = "\x1b[33m";
+inline constexpr std::string_view kReset  = "\x1b[0m";
+
+inline void printColored(std::string_view color, std::string_view prefix,
+                         std::string_view message, std::FILE* stream) {
+    std::fprintf(stream, "%.*s%.*s%.*s%.*s\n",
+                 static_cast<int>(color.size()),   color.data(),
+                 static_cast<int>(prefix.size()),  prefix.data(),
+                 static_cast<int>(message.size()), message.data(),
+                 static_cast<int>(kReset.size()),  kReset.data());
+}
+
+} // namespace log_detail
+
+template<typename... Args>
+void Error(formatting::format_string<Args...> fmt, Args&&... args) {
+    log_detail::printColored(log_detail::kRed, "Error: ",
+                             formatting::format(fmt, std::forward<Args>(args)...), stderr);
+}
+
+template<typename... Args>
+void Warning(formatting::format_string<Args...> fmt, Args&&... args) {
+    log_detail::printColored(log_detail::kYellow, "Warning: ",
+                             formatting::format(fmt, std::forward<Args>(args)...), stderr);
+}
+
+template<typename... Args>
+void Info(formatting::format_string<Args...> fmt, Args&&... args) {
+    const std::string msg = formatting::format(fmt, std::forward<Args>(args)...);
+    std::fprintf(stdout, "%.*s\n", static_cast<int>(msg.size()), msg.data());
+}
